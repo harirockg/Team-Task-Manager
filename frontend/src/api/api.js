700c67230d48dc212f8439bc32eb1948cf208e54
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2026-05-01 04:12:03 +0530

    updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // 👉 Agar env variable hai to use karega
 // 👉 nahi hai to fallback Render backend pe jayega
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://team-task-manager-9t58.onrender.com/api",
+  baseURL:"https://team-task-manager-9t58.onrender.com/api",
   withCredentials: true, // cookies support (important for auth)
 });
 
