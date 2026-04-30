00c5a3c63da0d8e82685e9ed08190aa016fea2fe
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2026-05-01 03:59:09 +0530

    frontend deploy config

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-9t58.onrender.com/api",
 });
 
 // TOKEN AUTO ADD
